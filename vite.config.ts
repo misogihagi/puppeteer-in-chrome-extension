0b8d2c54361488f6d004e05e9795fa09cf3cd4dd
misogihagi <47350059+misogihagi@users.noreply.github.com>
2024-12-01 01:11:00 +0900

    init crx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 export default defineConfig({
   plugins: [
@@ -10,6 +12,7 @@ export default defineConfig({
     */
     // devtools(),
     solidPlugin(),
+    crx({ manifest }),
   ],
   server: {
     port: 3000,
